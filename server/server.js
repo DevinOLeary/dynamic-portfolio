23b6fcd4e6ed027ab5e6a54ef0c4a9470dba53b7
Devin <djoleary07@gmail.com>
2017-11-12 15:42:03 -0500

    test

--- server/server.js
+++ server/server.js
@@ -14,13 +14,11 @@ const PORT = process.env.PORT || 3001;
 
 
 // Priority serve any static files.
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', (req, res) => {
-  res.sendFile('../client/build/index.html');
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // uncomment after placing your favicon in /public
