9722a5e635c502a558ec53d272ac088d7da52dd9
Devin <djoleary07@gmail.com>
2017-11-11 20:00:57 -0500

    test

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// view engine setup
+app.set('views', path.join(__dirname, 'client/build'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -29,6 +32,7 @@ app.use(fileUpload({
   limits: {fileSize: 20000000}
 }));
 
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
