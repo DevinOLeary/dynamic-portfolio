dd7a379d8315eacb7f133573e156eae0f6230cbc
Devin <djoleary07@gmail.com>
2017-11-12 15:23:33 -0500

    test

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 3001;
 
 
 // Priority serve any static files.
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
