cbd4d3664c96c50cb9a2efdf9d8c526bc2f648cf
Devin <djoleary07@gmail.com>
2017-11-13 18:48:12 -0500

    added git ignore and setup static file service

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //server client static files
-app.use(express.static(`../client/build`));
+app.use(express.static(`${__dirname}/client/build`));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
