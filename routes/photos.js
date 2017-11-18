25a51f5668b1b8e528b074472c55dcadde62d50b
Devin <djoleary07@gmail.com>
2017-11-17 21:53:30 -0500

    test

--- routes/photos.js
+++ routes/photos.js
@@ -42,11 +42,12 @@ router.get('/:category', (req, res) => {
     Bucket: BUCKET_NAME
   });
   Photo.find({category}).then((docs) => {
+    console.log(`open ${docs}`);
     if(!docs || docs.length === 0){
       return res.status(404).send('No Images Found');
     } else if(docs.length === 1){
       s3Bucket.createBucket(() => {
-        console.log(docs.image);
+        console.log(`single image ${docs}`);
         let params = {
           Bucket: BUCKET_NAME,
           Key: docs.image
