e329fb8323e6e0aa091ca97dca0ff5b2e22a3287
Devin <djoleary07@gmail.com>
2017-11-20 21:02:20 -0500

    test

--- client/src/components/presentational-components/projects/ProjectWindowDirectory.js
+++ client/src/components/presentational-components/projects/ProjectWindowDirectory.js
@@ -8,14 +8,15 @@ import SingleProjectContent from './SingleProjectContent';
 
 
 const ProjectWindowDirectory = (props) => {
+  console.log(props.singleProject);
   let contentWindow = ''
   if(props.singleProject && typeof props.singleProject != 'undefined'){
     let projectContent= '';
-    if(props.category === "Design"){
+    if(props.category === "design"){
       projectContent = <DesignProject {...props}/>
-    } else if(props.category === "Development"){
+    } else if(props.category === "development"){
       projectContent = <WebProject {...props}/>
-    } else if(props.category === "Animation"){
+    } else if(props.category === "animation"){
       projectContent = <AnimationProject {...props}/>
     } else{
       projectContent = '';
