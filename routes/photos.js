f600dbf70b122e185db4a06083761135be2dc53f
Devin <djoleary07@gmail.com>
2017-11-20 22:07:47 -0500

    added photo delete route and rebuilt client

--- routes/photos.js
+++ routes/photos.js
@@ -48,17 +48,15 @@ router.get('/:category', (req, res) => {
       return res.status(404).send('No Images Found');
     } else if(docs.length === 1){
       //return single photo
-      s3Bucket.createBucket(() => {
-        let params = {
-          Bucket: BUCKET_NAME,
-          Key: docs[0].image
+      let params = {
+        Bucket: BUCKET_NAME,
+        Key: docs[0].image
+      }
+      s3Bucket.getSignedUrl('getObject',params, (err, data) => {
+        if(err){
+          return res.status(400).send(err);
         }
-        s3Bucket.getSignedUrl('getObject',params, (err, data) => {
-          if(err){
-            return res.status(400).send(err);
-          }
-          res.send({data});
-        });
+        res.send({data});
       });
     } else if(docs.length > 1){
       //create an array of photo objects
@@ -86,6 +84,29 @@ router.get('/:category', (req, res) => {
     res.status(404).send(err);
   });
 });
+
+// router.delete('/:id', (req, res) => {
+//   const id = req.params.id;
+//   Photo.findById(id).then((doc) => {
+//     let s3Bucket = new AWS.S3({
+//       accessKeyId: process.env.AWS_ACCESS_ID,
+//       secretAccessKey: process.env.AWS_ACCESS_KEY,
+//       region: "us-east-1",
+//       Bucket: BUCKET_NAME
+//     });
+//     let params = {
+//       Bucket: BUCKET_NAME,
+//       Key: doc.image
+//     }
+//     s3Bucket.deleteObject(params, (err, data) => {
+//       if(err){
+//         res.status(400).send(err);
+//       } else {res.send(data);}
+//     });
+//   }).catch((err) => {
+//     res.status(404).send(err);
+//   });
+// });
 
 
 module.exports = router;
