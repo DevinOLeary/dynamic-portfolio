72ed1be67c92a52efb4bf1a644da7a5e7efb04dd
Devin <djoleary07@gmail.com>
2017-11-20 20:59:40 -0500

    test

--- client/src/components/presentational-components/projects/ProjectWindowDirectory.js
+++ client/src/components/presentational-components/projects/ProjectWindowDirectory.js
@@ -9,22 +9,22 @@ import SingleProjectContent from './SingleProjectContent';
 
 const ProjectWindowDirectory = (props) => {
   let contentWindow = ''
-    if(props.singleProject){
-      let projectContent= '';
-      if(props.category === "Design"){
-        projectContent = <DesignProject {...props}/>
-      } else if(props.category === "Development"){
-        projectContent = <WebProject {...props}/>
-      } else if(props.category === "Animation"){
-        projectContent = <AnimationProject {...props}/>
-      } else{
-        projectContent = '';
-      }
-    contentWindow = projectContent
+  if(props.singleProject && typeof props.singleProject != 'undefined'){
+    let projectContent= '';
+    if(props.category === "Design"){
+      projectContent = <DesignProject {...props}/>
+    } else if(props.category === "Development"){
+      projectContent = <WebProject {...props}/>
+    } else if(props.category === "Animation"){
+      projectContent = <AnimationProject {...props}/>
+    } else{
+      projectContent = '';
+    }
+  contentWindow = projectContent
   }
   return (
     <div className="full-width">
-      {contentWindow !== '' ?
+      {contentWindow != '' ?
       <SingleProjectContent {...props} closeProject={props.closeProject}>
         {contentWindow}
       </SingleProjectContent>
