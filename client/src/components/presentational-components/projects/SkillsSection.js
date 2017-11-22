bc5f626a058a066610a2ab7d306ae38d720d882b
Devin <djoleary07@gmail.com>
2017-11-22 12:56:50 -0500

    build

--- client/src/components/presentational-components/projects/SkillsSection.js
+++ client/src/components/presentational-components/projects/SkillsSection.js
@@ -3,28 +3,28 @@ import React from 'react';
 import animations from '../../small-components/animations';
 
 
-const SkillsSection = (props) => {
-
-  function loadAnimation(){
-    let skills = this.skills.querySelectorAll('.img-skills');
+class SkillsSection extends React.Component{
+  loadAnimation(){
+    const skills = this.skills.querySelectorAll('.img-skills');
     animations.fadeInList(skills);
   }
-
-  let list = props.skillsArray.map((skill) => {
-    return <li key={skill.id}>
-              <img src={skill.data} alt={skill.category} className="img-skill" onLoad={loadAnimation} />
-            </li>
-  });
-  return (
-      <article className="flex-container center column full-width ">
-        <div className="flex-container center">
-          <p className="content-body_read">Technology is changing every day, and it really requires a passion that's borderline obsession to stay up to date on the most effective ways to build modern websites and applications. There's no telling what I'll be using next month, but here are a few of the tools I'm currently working with:</p>
-        </div>
-        <ul className="flex-container row center" ref={(ref) => {this.skills = ref}}>
-          {list}
-        </ul>
-      </article>
-  );
+  render(props){
+    let list = this.props.skillsArray.map((skill) => {
+      return <li key={skill.id}>
+                <img src={skill.data} alt={skill.category} className="img-skill" onLoad={this.loadAnimation} />
+              </li>
+    });
+    return (
+        <article className="flex-container center column full-width ">
+          <div className="flex-container center">
+            <p className="content-body_read">Technology is changing every day, and it really requires a passion that's borderline obsession to stay up to date on the most effective ways to build modern websites and applications. There's no telling what I'll be using next month, but here are a few of the tools I'm currently working with:</p>
+          </div>
+          <ul className="flex-container row center" ref={(ref) => {this.skills = ref}}>
+            {list}
+          </ul>
+        </article>
+    );
+  }
 }
 
 
