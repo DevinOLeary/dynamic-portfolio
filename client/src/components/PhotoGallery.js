f82517a1c9cfa86ada0a5cda0053d3798e8488cf
Devin <djoleary07@gmail.com>
2017-11-26 09:56:47 -0500

    build

--- client/src/components/PhotoGallery.js
+++ client/src/components/PhotoGallery.js
@@ -81,9 +81,9 @@ class PhotoGallery extends React.Component {
           </hgroup>
           <section className="flex-container center column">
             <ul ref={ref => this.gallery = ref} onLoad={this.handleImageLoad.bind(this)}>
-              {this.state.loading ? <LoadingPane/> :
+
                 {list}
-              }
+              
             </ul>
           </section>
         </main>
