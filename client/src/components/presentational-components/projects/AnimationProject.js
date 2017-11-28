4ca32a0a1a14efb74191276a43bd512cdb67e73b
Devin <djoleary07@gmail.com>
2017-11-27 19:15:20 -0500

    test

--- client/src/components/presentational-components/projects/AnimationProject.js
+++ client/src/components/presentational-components/projects/AnimationProject.js
@@ -16,8 +16,12 @@ const AnimationProject = () => {
         <aside className="project_preview-slider flex-container center">
           <ProjectSlider>
             <div>
-              <p data-height="265" data-theme-id="0" data-slug-hash="gXbNpv" data-default-tab="result" data-user="djoleary07" data-embed-version="2" data-pen-title="Sunset" class="codepen">See the Pen <a href="https://codepen.io/djoleary07/pen/gXbNpv/">Sunset</a> by Devin OLeary (<a href="https://codepen.io/djoleary07">@djoleary07</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+              <p data-height="477" data-theme-id="0" data-slug-hash="gXbNpv" data-default-tab="result" data-user="djoleary07" data-embed-version="2" data-pen-title="Sunset" class="codepen">See the Pen <a href="https://codepen.io/djoleary07/pen/gXbNpv/">Sunset</a> by Devin OLeary (<a href="https://codepen.io/djoleary07">@djoleary07</a>) on <a href="https://codepen.io">CodePen</a>.</p>
               <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
+            </div>
+            <div>
+              <iframe height='477' scrolling='no' title='Sunset' src='//codepen.io/djoleary07/embed/gXbNpv/?height=477&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin O'Leary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
+              </iframe>
             </div>
           </ProjectSlider>
         </aside>
