236b60efa581f712d7751814a304861e55dcf4cc
Devin <djoleary07@gmail.com>
2017-11-28 12:59:40 -0500

    test

--- client/src/components/presentational-components/projects/AnimationProject.js
+++ client/src/components/presentational-components/projects/AnimationProject.js
@@ -13,7 +13,7 @@ const AnimationProject = () => {
         <hr className="divider-colored"/>
       </hgroup>
       <section className="flex-container center">
-
+        <div className="animation-container">
           <ProjectSlider>
             <div className="animation-project_preview slide-item">
               <iframe height='100%' scrolling='no' title='Sunset' src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=265&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
@@ -24,7 +24,7 @@ const AnimationProject = () => {
               </iframe>
             </div>
           </ProjectSlider>
-
+        </div>
       </section>
     </div>
   );
