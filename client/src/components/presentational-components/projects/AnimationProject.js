c8253b1ff423b1467f6985eb9f7d33cc708b8192
Devin <djoleary07@gmail.com>
2017-11-27 20:10:41 -0500

    test

--- client/src/components/presentational-components/projects/AnimationProject.js
+++ client/src/components/presentational-components/projects/AnimationProject.js
@@ -15,16 +15,16 @@ const AnimationProject = () => {
       <section className="flex-container center">
 
           <ProjectSlider>
-            <div className="flex-container center">
-              <iframe height='477' scrolling='no' title='Sunset' className="img-project_preview" src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=477&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
+            <div className="flex-container center img-project_preview">
+              <iframe height='477' scrolling='no' title='Sunset' src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=477&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
               </iframe>
             </div>
-            <div className="flex-container center">
-              <iframe height='477' scrolling='no' title='Sunset' className="img-project_preview" src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=477&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
+            <div className="flex-container center img-project_preview">
+              <iframe height='477' scrolling='no' title='Sunset' src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=477&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
               </iframe>
             </div>
           </ProjectSlider>
-    
+
       </section>
     </div>
   );
