e9c38b090d40eae1205bc25ac7d6fd5f2f69b33b
Devin <djoleary07@gmail.com>
2017-12-04 11:56:19 -0500

    build

--- client/src/components/presentational-components/about/TimelineHeader.js
+++ client/src/components/presentational-components/about/TimelineHeader.js
@@ -8,7 +8,7 @@ const coloredUnderline = {
 const TimelineHeader = (props) => {
   if(props.aboutInfo.length === 0){return null}
   const timelineHeader = props.aboutInfo.map(head => (
-    <li className={"timeline-header hover-highlight" + head._id === props.timePeriodId ? 'active' : 'default'} key={head._id} id={head._id} onClick={props.loadTime.bind(this, head._id)}>
+    <li className={`timeline-header hover-highlight ${head._id === props.timePeriodId ? 'active' : 'default'}`} key={head._id} id={head._id} onClick={props.loadTime.bind(this, head._id)}>
       <h4>{head.header}</h4>
       <hr className="link-underline" style={coloredUnderline}/>
     </li>
