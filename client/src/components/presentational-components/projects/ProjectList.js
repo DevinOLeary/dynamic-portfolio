7da7ceacae34a874f2fa75437460e9864507cb0e
Devin <djoleary07@gmail.com>
2017-12-05 12:45:10 -0500

    design build

--- client/src/components/presentational-components/projects/ProjectList.js
+++ client/src/components/presentational-components/projects/ProjectList.js
@@ -9,7 +9,7 @@ import ListItems from './ListItems';
 const ProjectList = (props) => {
   return (
     <div className="flex-container center full-width">
-    <div className="triangle-left"></div>
+    <div className="triangle"></div>
       <section className="flex-container center row open_content-container">
         <ul className="flex-container row center full-width">
           <li className="project_cat-header"><CategoryHeader title='development' {...props} onClick={props.updateCategoryList}/></li>
--- client/src/components/small-components/LoadingPane.js
+++ client/src/components/small-components/LoadingPane.js
@@ -3,7 +3,7 @@ import treeIcon from '../../images/tree_icon.png';
 
 const LoadingPane = (props) => (
     <div className="body-container flex-container center">
-      <img src={treeIcon} alt="one sec..." className="loading-icon"/>
+      
     </div>
 )
 
