f563fc9fd4c967bae996933b78f835bab67762ed
Devin <djoleary07@gmail.com>
2017-12-06 18:27:06 -0500

    build

--- client/src/components/presentational-components/about/AboutContent.js
+++ client/src/components/presentational-components/about/AboutContent.js
@@ -58,14 +58,14 @@ const AboutContent = (props) => {
                 <div className="inner-box flex-container center column">
                   <h3>biking:</h3>
                   <p className="content-body_read">Mountain, road, gravel, town, unicycle.
-                  Riding them and fixing them up.</p>
+                  I love riding them and fixing them up. Being able to ride my bikes in cool places and with cool people ties into pretty much all the things that I see as important in this world, namely nature preservation, community, and health, and tech is the powerful tool that can bring it all together</p>
                   <br/>
                   <h3>coffee shops:</h3>
                   <p className="content-body_read">Coffee is ok, but hanging out in cool coffee shops
                   with good vibes is one of the top reasons I migrated to computer work.</p>
                   <h3>community:</h3>
                   <p className="content-body_read">I get super stoked about people doing cool things to bring
-                  people together in their community. My life mission is to
+                  everyone together in their community. My life mission is to
                   maximize my role in creating community, and using the web to extend the range infinitely.</p>
                 </div>
               </article>
