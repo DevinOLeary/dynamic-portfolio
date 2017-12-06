24564f4c6f52b25fbfabd3a6d84d07065189ac4d
Devin <djoleary07@gmail.com>
2017-12-05 22:58:06 -0500

    build

--- client/src/components/presentational-components/projects/AnimationProject.js
+++ client/src/components/presentational-components/projects/AnimationProject.js
@@ -25,7 +25,7 @@ const AnimationProject = () => {
               <iframe height='100%' scrolling='no' title='Sunset' src='//codepen.io/djoleary07/embed/preview/gXbNpv/?height=265&theme-id=0&default-tab=result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style={iframeStyle}>See the Pen <a href='https://codepen.io/djoleary07/pen/gXbNpv/'>Sunset</a> by Devin OLeary (<a href='https://codepen.io/djoleary07'>@djoleary07</a>) on <a href='https://codepen.io'>CodePen</a>.
               </iframe>
             </div>
-            <div className="animation-project flex-container center">
+            <div className="animation-project_preview slide-item">
               <iframe src="https://codesandbox.io/embed/9z0j727vwp?view=preview" style={sandBoxStyle} sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
             </div>
           </ProjectSlider>
