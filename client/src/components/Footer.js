248920517f275826734d2bd43cde4213ec4d108f
Devin <djoleary07@gmail.com>
2017-12-11 18:22:36 -0500

    build

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,21 +6,24 @@ import mail from '../images/mail-icon.png';
 
 
 const Footer = () => {
+  const clearBackground = {
+    backgroundColor: 'rgba(240, 89, 65, 0)'
+  }
   return(
     <footer className="footer-container flex-container center column text-inverse">
-      <h3>get in touch</h3>
+      <h3 style={clearBackground}>get in touch</h3>
       <br/>
-      <a href="mailto:djoleary07@gmail.com"><img src={mail} alt="djoleary07@gmail.com" className='img-social'/></a>
+      <a href="mailto:djoleary07@gmail.com" style={clearBackground}><img src={mail} alt="djoleary07@gmail.com" className='img-social'/></a>
       <br/>
       <ul className="flex-container row center">
-        <li><a href="https://github.com/djoleary007" target="_blank" rel="noopener noreferrer"><img src={github} alt='github' className='img-social' /></a></li>
+        <li><a href="https://github.com/djoleary007" target="_blank" rel="noopener noreferrer" style={clearBackground}><img src={github} alt='github' className='img-social' /></a></li>
         <br/>
-        <li><a href="https://www.instagram.com/devin_oleary/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt='instagram' className='img-social'/></a></li>
+        <li><a href="https://www.instagram.com/devin_oleary/" target="_blank" rel="noopener noreferrer" style={clearBackground}><img src={instagram} alt='instagram' className='img-social'/></a></li>
       </ul>
       <br/>
       <hr className="divider-colored"/>
       <br/>
-      <h4>bloomington, indiana</h4>
+      <h4 style={clearBackground}>bloomington, indiana</h4>
     </footer>
 
   );
