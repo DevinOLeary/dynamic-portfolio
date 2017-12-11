7f9d7eae2e2544ef18e6c1f39f82ab83c25a490e
Devin <djoleary07@gmail.com>
2017-12-11 18:39:51 -0500

    build

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,24 +5,22 @@ const whiteUnderline = {
   backgroundColor: `rgba(240, 89, 65, .6)`,
   borderColor: `rgba(240, 89, 65, .1)`
 }
-const clearBackground = {
-  backgroundColor: 'rgba(240, 89, 65, 0)'
-}
+
 
 const Nav = () => {
     return(
       <nav className="full-width forefront">
         <ul className="flex-container nav-bar row text-inverse">
           <li className="hover-highlight">
-            <NavLink to="/photography" style={clearBackground}>photography</NavLink>
+            <NavLink to="/photography" className="text-inverse">photography</NavLink>
             <hr className='link-underline' style={whiteUnderline}/>
           </li>
           <li className="hover-highlight">
-            <NavLink to="/dev&design" style={clearBackground}>development & design</NavLink>
+            <NavLink to="/dev&design" className="text-inverse">development & design</NavLink>
             <hr className='link-underline' style={whiteUnderline}/>
           </li>
           <li className="hover-highlight">
-            <NavLink to="/about" style={clearBackground}>about me</NavLink>
+            <NavLink to="/about" className="text-inverse">about me</NavLink>
             <hr className='link-underline' style={whiteUnderline}/>
           </li>
         </ul>
--- client/src/components/small-components/MobileMenu.js
+++ client/src/components/small-components/MobileMenu.js
@@ -9,19 +9,19 @@ const whiteUnderline = {
 
 const MobileMenu = (props) => (
     <div className={`mobile_menu-container ${props.openStatus ? 'menu-open' : 'menu-close'}`}>
-      <a onClick={props.toggleMenu} className="button-exit">exit</a>
+      <a onClick={props.toggleMenu} className="button-exit text-inverse">exit</a>
       <ul className="flex-container top-center column">
-      <li className="text-inverse hover-highlight" onClick={props.toggleMenu}>
-        <NavLink exact to="/"><h3>home</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
+      <li className="hover-highlight" onClick={props.toggleMenu}>
+        <NavLink exact to="/"><h3 className="text-inverse">home</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
       </li>
-      <li className="text-inverse hover-highlight" onClick={props.toggleMenu}>
-        <NavLink to="/photography"><h3>photography</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
+      <li className="hover-highlight" onClick={props.toggleMenu}>
+        <NavLink to="/photography"><h3 className="text-inverse">photography</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
       </li>
-      <li className="text-inverse hover-highlight" onClick={props.toggleMenu}>
-        <NavLink to="/dev&design"><h3>development & design</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
+      <li className="hover-highlight" onClick={props.toggleMenu}>
+        <NavLink to="/dev&design"><h3 className="text-inverse">development & design</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
       </li>
-      <li className="text-inverse hover-highlight" onClick={props.toggleMenu}>
-        <NavLink to="/about"><h3>about me</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
+      <li className="hover-highlight" onClick={props.toggleMenu}>
+        <NavLink to="/about"><h3 className="text-inverse">about me</h3><hr className='link-underline' style={whiteUnderline}/></NavLink>
       </li>
       </ul>
     </div>
