35104c2557579187e12809ba9f46229a2a6728ae
Devin <djoleary07@gmail.com>
2017-12-15 17:55:29 -0500

    build

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Provider} from 'mobx-react';
 import {BrowserRouter} from 'react-router-dom';
+import DocumentMeta from 'react-document-meta';
 
 
 //components
@@ -18,9 +19,14 @@ import RootStore from './stores/RootStore';
 
 class App extends Component {
   render() {
+      const meta = {
+        title: 'Devin O&#8217;Leary Web Development and Design',
+        description: 'I&#8217;m Devin O&#8217;Leary, a Web Developer and Designer, Photographer, Cyclist, Surfer, Adventurer, and really just someone trying to live an authentic and impactful life. Send me a message so we can talk about creating something great!'
+      };
       const stores = new RootStore();
     return (
       <Provider store={stores}>
+          <DocumentMeta {...meta}/>
           <BrowserRouter>
             <ScrollToTop>
               <Main />
